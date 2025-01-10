1e3f3899a4589d10505d71139fff6203e4885d0f
OLUWANIYIAKIN <olu24002@byui.edu>
2025-01-11 00:20:22 +0100

    ss

--- server.js
+++ server.js
@@ -6,10 +6,13 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
