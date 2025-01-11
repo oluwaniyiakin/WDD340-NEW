80baddd8e706dd0f02328812d2050e432badbdfb
OLUWANIYIAKIN <olu24002@byui.edu>
2025-01-11 16:08:56 +0100

    ss

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public')); // Serve the public folder
 
 // Route handler for the home page
 app.get('/', (req, res) => {
