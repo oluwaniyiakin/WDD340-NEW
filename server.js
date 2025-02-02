beafa898ec8af57105f686a482068f1de2fef971
OLUWANIYIAKIN <olu24002@byui.edu>
2025-02-02 01:33:06 +0100

    hh

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Parsing form data
 
 // Set EJS as the templating engine
-app.set("views", path.join(__dirname, "src", "views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 
